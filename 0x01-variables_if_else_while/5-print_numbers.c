ed8cdecce55648caf3e350f0d4c4a4df0044557b
root <jisheunesu97>
2023-08-13 12:42:46 -0700

    print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description - Print all single digit numbers of base 10 from 0
+*The whole code is contained in the main function
+*Return: 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+
+putchar('\n');
+return (0);
+}
