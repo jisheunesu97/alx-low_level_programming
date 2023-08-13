ac794d56899889b7c6a2784829d9762dea78c65a
root <jisheunesu97>
2023-08-13 02:14:37 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+	/* Declare a variable*/
+	char alphabet = 'a';
+
+	while(alphabet <= 'z') {
+	/* print the value of the variable*/
+		putchar(alphabet);
+
+	/* increment the variable ('a becomes b')*/
+		alphabet++;
+	}
+
+	/* print a new line*/
+	putchar('\n');
+	return (0);
+}
