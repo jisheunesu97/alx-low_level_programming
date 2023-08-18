342118ea40ea3f2cb2bec43546f251264bfd2d35
root <jisheunesu97>
2023-08-18 00:22:27 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,20 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
-printf("FizzBuzzz");
+printf("FizzBuzz");
 
 else if (i % 3 == 0)
 printf("Fizz");
-
+ 
 else if (i % 5 == 0)
 printf("Buzz");
 
 else
 printf("%i", i);
-
+ 
 if (i < 100)
 printf(" ");
 }
 printf("\n");
-
 return (0);
 }
