e3a98e71ed11e67f0051371ac16b106b5e748f6f
root <jisheunesu97>
2023-09-11 23:03:20 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _stdrup - function that returns a pointer to a newly
+* allocated space in memory
+* @str: is char
+*Return: 0 Success
+*
+*/
+
+char *_stdrup(char *str)
+{
+char *mem;
+int j, k = 0;
+
+if (str == NULL)
+return (NULL);
+
+j = 0;
+while (str[j] != '\0')
+j++;
+
+mem = malloc(sizeof(char) * (j + 1));
+
+if (mem == NULL)
+return (NULL);
+
+for (k = 0; str[k]; k++)
+mem[k] = str[k];
+
+return (mem);
+}
