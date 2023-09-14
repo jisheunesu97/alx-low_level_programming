8ee289a604be8245f3a64ac8f0971d2817d64864
jisheunesu97 <ishejchiurawa@gmail.com>
2023-09-14 17:54:39 +0200

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VAR_FUNCTIONS_FILE
+#define VAR_FUNCTIONS_FILE
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
